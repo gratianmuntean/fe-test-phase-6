5250bfec0478b16e4c416a9287d5c03afb9c74e3
sebiarion <sebiarion@yahoo.com>
2022-02-17 15:59:16 +0200

    sebi ex

--- src/ex1/index.js
+++ src/ex1/index.js
@@ -1,47 +1,62 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-import '../App.css';
+import '../App.css'
 
-//1. In the first exercise you have to increment the parent count state from the child. 
+//1. In the first exercise you have to increment the parent count state from the child.
 //2. After you finish 1. use the useEffect to check when the count changes, and  console.log if the value it's odd or even.
 
 const Ex01Requirements = () => {
   return (
     <>
-      <code style={{ 'width': '60%' }}>
-        1. In the first exercise you have to increment the parent count state from the child.
+      <code style={{ width: '60%' }}>
+        1. In the first exercise you have to increment the parent count state
+        from the child.
       </code>
       <br />
-      <code style={{ 'width': '60%' }}>
-        2. After you finish 1. use the useEffect to check when the count changes, and console.log if the value it's odd or even.
+      <code style={{ width: '60%' }}>
+        2. After you finish 1. use the useEffect to check when the count
+        changes, and console.log if the value it's odd or even.
       </code>
     </>
   )
 }
 
 const Ex01 = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+  useEffect(() => {
+    if (count > 0) {
+      if (count % 2 == 0) {
+        console.log(`${count}: Value it's even `)
+      } else {
+        console.log(`${count}: Value it's odd `)
+      }
+    }
+  })
 
   return (
-    <div className="App-header">
+    <div className='App-header'>
       <Ex01Requirements />
-      <Link to="/2">Go to exercise 02</Link>
+      <Link to='/2'>Go to exercise 02</Link>
       <h1>Ex 01</h1>
 
-
       <h1>{count}</h1>
-      <ChildComponent />
+      <ChildComponent increment={increment} />
     </div>
   )
 }
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
+  const { increment } = props
   return (
-    <button style={{ cursor: 'pointer' }}>
+    <button style={{ cursor: 'pointer' }} onClick={increment}>
       Add +1
     </button>
   )
 }
 
-export default Ex01;
+export default Ex01
--- src/ex2/index.js
+++ src/ex2/index.js
@@ -1,42 +1,82 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
-import '../App.css';
+import '../App.css'
 
-const BASE_URL = 'https://jsonplaceholder.typicode.com/users';
+const BASE_URL = 'https://jsonplaceholder.typicode.com/users'
 
 const Ex02Requirements = () => {
   return (
     <>
-      <code style={{ 'width': '60%' }}>
-        1. using the BASE_URL endpoint get the users and display them in a unordered list by "name username";
-        hint: use the useState, useEffect hooks / map function.
+      <code style={{ width: '60%' }}>
+        1. using the BASE_URL endpoint get the users and display them in a
+        unordered list by "name username"; hint: use the useState, useEffect
+        hooks / map function.
       </code>
       <br />
-      <code style={{ 'width': '60%' }}>
-        2. after finishing the 1. add a text input and filter the displayed users list by first name or last name.
-        hint: use on change event / useState/ filter function.
+      <code style={{ width: '60%' }}>
+        2. after finishing the 1. add a text input and filter the displayed
+        users list by first name or last name. hint: use on change event /
+        useState/ filter function.
       </code>
     </>
   )
 }
 
 const Ex02 = () => {
-  //1. using the BASE_URL endpoint get the users and display them in a unordered list by "name username";
-  // hint: use the useState, useEffect hooks / map function.
-  // 2. after finishing the 1. add a text input and filter the displayed users list by first name or last name.
-  // hint: use on change event / useState/ filter function.
+  const [users, setUsers] = useState('')
+
+  const [foundUsers, setFoundUsers] = useState([])
+
+  const fetchUsers = async () => {
+    const response = await fetch(BASE_URL)
+    return response.json()
+  }
+
+  useEffect(() => {
+    fetchUsers().then((data) => setFoundUsers(data))
+  }, [])
+
+  const filter = (e) => {
+    const keyword = e.target.value
+
+    if (keyword !== '') {
+      const results = foundUsers.filter((user) => {
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase())
+      })
+      setFoundUsers(results)
+    } else {
+      fetchUsers().then((data) => setFoundUsers(data))
+    }
+
+    setUsers(keyword)
+  }
+
   return (
-    <div className="App-header">
+    <div className='App-header'>
       <Ex02Requirements />
-      <Link to="/1">Go to exercise 01</Link>
+      <Link to='/1'>Go to exercise 01</Link>
       <h1>Ex 02</h1>
 
-
-      <input type='text' />
+      <input type='search' onChange={filter} value={users} />
       <h2>Here comes the list</h2>
+      <ul>
+        {foundUsers.map((user, index) => {
+          return (
+            <li key={index}>
+              <span>{user.name} | </span>
+              <span>{user.username}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
 
-export default Ex02;
+export default Ex02
+
+//1. using the BASE_URL endpoint get the users and display them in a unordered list by "name username";
+// hint: use the useState, useEffect hooks / map function.
+// 2. after finishing the 1. add a text input and filter the displayed users list by first name or last name.
+// hint: use on change event / useState/ filter function.
