699c2dbc228adeadcef997c331e0a04b16660a40
kocsis_katalin <k.kocsis@camelweb.com>
2022-02-17 16:11:47 +0200

    Test Phase 6

--- src/ex1/index.js
+++ src/ex1/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import '../App.css';
@@ -22,24 +22,43 @@ const Ex01Requirements = () => {
 
 const Ex01 = () => {
   const [count, setCount] = useState(0);
+  const increment = () => {
+    setCount(count + 1)
+  }
+  const zero = () => {
+    setCount(0)
+  }
+
+  useEffect(() => { count%2==0? console.log(`${count} is even`):console.log(`${count} is odd`);
+  }, [count]);
 
   return (
     <div className="App-header">
       <Ex01Requirements />
       <Link to="/2">Go to exercise 02</Link>
-      <h1>Ex 01</h1>
-
-
+      <h1>Ex 01 Katica ;) </h1>
+  
       <h1>{count}</h1>
-      <ChildComponent />
+      <ChildComponent increaseOnClick={increment} count={count} /> 
+      <p>Would you like to start from zero again? :) </p>
+      <DeleteComponent deleteOnClick={zero} count={zero} /> 
     </div>
   )
 }
 
-const ChildComponent = () => {
+const ChildComponent = ({ increaseOnClick }) => {
   return (
-    <button style={{ cursor: 'pointer' }}>
+    <button onClick={increaseOnClick} style={{ cursor: 'pointer' }}>
       Add +1
+    </button>
+  )
+}
+
+
+const DeleteComponent = ({ deleteOnClick }) => {
+  return (
+    <button onClick={deleteOnClick} style={{ cursor: 'pointer' }}>
+      Yes!!!
     </button>
   )
 }
--- src/ex2/index.js
+++ src/ex2/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import '../App.css';
@@ -26,15 +26,40 @@ const Ex02 = () => {
   // hint: use the useState, useEffect hooks / map function.
   // 2. after finishing the 1. add a text input and filter the displayed users list by first name or last name.
   // hint: use on change event / useState/ filter function.
+  const [users, setUsers] = useState([]);
+  const [filter, setFilter] = useState('');
+  const fetchUsers = async () => {
+      return await fetch(BASE_URL).then((res) => res.json());
+  }
+
+  useEffect(() => {
+      fetchUsers().then(data => setUsers(data));
+  }, [])
+
   return (
     <div className="App-header">
       <Ex02Requirements />
       <Link to="/1">Go to exercise 01</Link>
-      <h1>Ex 02</h1>
+      <h1>Ex 02 Katica ;) </h1>
 
 
-      <input type='text' />
+      <input type='text' 
+                    id="filter" 
+                    name="filter" 
+                    value={filter}
+                    onChange={event => setFilter(event.target.value)}/>
+
       <h2>Here comes the list</h2>
+      <ul>
+              {users.filter(user=> user?.name.includes(filter) || user?.name.toLowerCase().includes(filter)|| filter === '').map((user,index) => {
+                  return (
+                      <li key={index}>
+                          <span>{user?.name} - </span>
+                          <span>{user?.username}</span>
+                      </li>
+                  )
+              })}
+          </ul>
     </div>
   )
 }
